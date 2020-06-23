d81217324c5e795aeda63d8285f0436b2a170c0c
maurya20 <mukesh.ice17@gmail.com>
2020-06-23 19:04:38 +0530

    Register page desigened and logo changed.

--- customer/urls.py
+++ customer/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('home', views.home, name='home'),
     path('register', views.register, name='register'),
     path('userlogin', views.userlogin, name='userlogin'),
     path('profile', views.Profile, name='profile'),
