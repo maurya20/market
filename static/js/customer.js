08dff937ea2c8e439150b84fc7fdb04ce158df3f
maurya20 <mukesh.ice17@gmail.com>
2020-07-15 10:29:22 +0530

    javascript confirm cancel implemented before going to edit profile. Same method can be used to ask confermation befor deleting anything.

---
+++ static/js/customer.js
@@ -0,0 +1,8 @@
+function myEdit() {
+    var result = confirm("Hey! are you sure to  edit your profile!");
+        if (result)
+            return true;
+        else
+            return false;
+          }
+    
